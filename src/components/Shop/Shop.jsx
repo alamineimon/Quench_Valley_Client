707ae455698a77d389e6d2302fccde0fc401b742
eimonalamin <eimonalamin@gmail.com>
2023-01-21 12:18:21 +0600

    dropdown profile modicfy

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card/Card";
 import Spinner from "../Spinner/Spinner";
 
